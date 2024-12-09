7c07130a6fef97a367550aa68fe67e96acd3e64f
Md. Shahedul Islam <mdshahedulislamsaim@gmail.com>
2024-12-09 20:18:32 +0600

    Add files via upload

---
+++ 2758.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main ()
+{
+
+	float a, b;
+	double c, d;
+	
+	a=1.2;
+	b=3.45;
+	c=3.451;
+	d=3.4516;
+
+	scanf("%f %f", &a, &b);
+	scanf("%lf %lf", &c, &d);
+
+	printf("A = %f, B = %f\n", a, b); // 7
+	printf("C = %lf, D = %lf\n", c, d); // 8
+	printf("A = %.1f, B = %.1f\n", a, b); // 9
+	printf("C = %.1lf, D = %.1lf\n", c, d); // 10
+	printf("A = %.2f, B = %.2f\n", a, b); // 11
+	printf("C = %.2lf, D = %.2lf\n", c, d); // 12
+	printf("A = %.3f, B = %.3f\n", a, b); // 13
+	printf("C = %.3lf, D = %.3lf\n", c, d); // 14
+	printf("A = %.3E, B = %.3E\n", a, b); // 15
+	printf("C = %.3E, D = %.3E\n", c, d); // 16
+	printf("A = %.0f, B = %.0f\n", a, b); // 17
+	printf("C = %.0lf, D = %.0lf\n", c, d); // 18
+
+}
