9e5a3fa8da5e36cef34eadf3b6ace0d86b186e7a
Md. Shahedul Islam <mdshahedulislamsaim@gmail.com>
2024-12-17 01:53:02 +0600

    Add files via upload

---
+++ 1014.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+ 
+    int X;
+    double Y,z;
+    
+    cin>>X>>Y;
+    
+    z = X/Y;
+    
+    
+    
+    printf("%.3lf km/l\n", z);
+    
+ 
+    return 0;
+}
