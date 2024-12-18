6c40497948ff52edebe7d0d401716f8b5ecca19e
Md. Shahedul Islam <mdshahedulislamsaim@gmail.com>
2024-12-19 01:29:34 +0600

    Add files via upload

---
+++ 1005.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+ using namespace std;
+int main() {
+    
+    double a,b,m;
+    float c=3.5,d=7.5;
+    cin>>a>>b;
+    m =((a*c)+(b*d))/(c+d);
+    
+    printf("MEDIA = %.5lfn",m );
+ 
+  
+ 
+    return 0;
+}
