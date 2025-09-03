e5569f767e7aa59a08d27c2e9c52430da2dae69d
Md. Shahedul Islam <mdshahedulislamsaim@gmail.com>
2025-09-03 16:58:26 +0600

    2987.cpp

--- 2987.cpp
+++ 2987.cpp
@@ -2,17 +2,14 @@
 
 int main()
 {
+    char n;
+    scanf("%c",&n);
 
-    char c;
-    int i, j;
-    char str[27];
-
-    for (i = 'A', j = 1; i <= 'Z'; ++i, ++j)
-        str[i] = j;
-
-    scanf("%c", &c);
-    printf("%d\n", str[c]);
-
-    return 0;
+    if(n >= 'A' && n <= 'Z')
+        printf("%d\n", n - 'A' + 1);
+    else if(n >= 'a' && n <= 'z')
+        printf("%d\n", n - 'a' + 1);
 
 }
+
+
