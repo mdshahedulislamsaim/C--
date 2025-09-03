c0cefdb0307c59c863a7e0be9db6a49aa3f5550d
Md. Shahedul Islam <mdshahedulislamsaim@gmail.com>
2025-09-03 22:20:04 +0600

    1021.cpp

--- 1021.cpp
+++ 1021.cpp
@@ -1,28 +1,64 @@
-#include <iostream>
- using namespace std;
-int main() {
- 
-    float n;
-    int a,b,c,d,e,f,g,h,i,j,k,l;
-    cin>>n;
-    
-    a = n/100;
-    b = (n-a*100)/50;
-    c = (n-a*100-b*50)/20;
-    d = (n-a*100-b*50-c*20)/10;
-    e = (n-a*100-b*50-c*20-d*10)/5;
-    f = (n-a*100-b*50-c*20-d*10-e*5)/2;
-    
-    cout<<"NOTAS:\n"<<a<<" nota(s) de R$ 100.00\n"<<b<<" nota(s) de R$ 50.00\n"<<c<<" nota(s) de R$ 20.00\n"<<d<<" nota(s) de R$ 10.00\n"<<e<<" nota(s) de R$ 5.00\n"<<f<<" nota(s) de R$ 2.00\n";
-    
-    g = (n-a*100-b*50-c*20-d*10-e*5-f*2);
-    h = ((n-a*100-b*50-c*20-d*10-e*5-f*2-g)*100)/50;
-    i = (((n-a*100-b*50-c*20-d*10-e*5-f*2-g)*100)-h*50)/25;
-    j = (((n-a*100-b*50-c*20-d*10-e*5-f*2-g)*100)-h*50-i*25)/10;
-    k = (((n-a*100-b*50-c*20-d*10-e*5-f*2-g)*100)-h*50-i*25-j*10)/5;
-    l = ((n-a*100-b*50-c*20-d*10-e*5-f*2-g)*100)-h*50-i*25-j*10-k*5;
-    
-    cout<<"MOEDAS:\n"<<g<<" moeda(s) de R$ 1.00\n"<<h<<" moeda(s) de R$ 0.50\n"<<i<<" moeda(s) de R$ 0.25\n"<<j<<" moeda(s) de R$ 0.10\n"<<k<<" moeda(s) de R$ 0.05\n"<<l<<" moeda(s) de R$ 0.01\n";
- 
-    return 0;
+#include <stdio.h>
+
+int main()
+{
+    double a;
+    int m,h,n,n1,n2,ft,tw,te,fv,two,o,coin,tv;
+    scanf("%lf", &a);
+
+    n = (int)a;
+    n1 = (int)a*100;
+    a = a*100;
+    n2 = (int)a;
+    coin = n2-n1;
+
+    printf("NOTAS:\n");
+
+    h = n / 100;
+    m = n % 100;
+    printf("%d nota(s) de R$ 100.00\n", h);
+
+    ft = m/50;
+    m = m%50;
+    printf("%d nota(s) de R$ 50.00\n", ft);
+
+    tw = m/20;
+    m = m%20;
+    printf("%d nota(s) de R$ 20.00\n", tw);
+
+    te = m/10;
+    m = m%10;
+    printf("%d nota(s) de R$ 10.00\n", te);
+
+    fv = m/5;
+    m = m%5;
+    printf("%d nota(s) de R$ 5.00\n", fv);
+
+    two = m/2;
+    m = m%2;
+    printf("%d nota(s) de R$ 2.00\n", two);
+
+    printf("MOEDAS:\n");
+
+    printf("%d moeda(s) de R$ 1.00\n", m);
+
+    ft = coin/50;
+    m = coin%50;
+    printf("%d moeda(s) de R$ 0.50\n", ft);
+
+    tv = m/25;
+    m = m%25;
+    printf("%d moeda(s) de R$ 0.25\n", tv);
+
+    te = m/10;
+    m = m%10;
+    printf("%d moeda(s) de R$ 0.10\n", te);
+
+    fv = m/5;
+    m = m%5;
+    printf("%d moeda(s) de R$ 0.05\n", fv);
+
+
+    printf("%d moeda(s) de R$ 0.01\n", m);
+
 }
