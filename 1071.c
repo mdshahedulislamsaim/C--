85a703b9433c8bd3c4969138cd2f3e6aa88826ae
Md. Shahedul Islam <mdshahedulislamsaim@gmail.com>
2025-09-06 14:50:58 +0600

    Add files via upload

---
+++ 1071.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, temp, sum = 0;
+    scanf("%d%d", &a, &b);
+
+    if (a > b)
+    {
+        temp = a;
+        a = b;
+        b = temp;
+    }
+
+    for (int i = a+1; i < b; i++)
+    {
+        if (i % 2 != 0)
+            sum += i;
+    }
+
+    printf("%d\n", sum);
+}
