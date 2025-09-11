7e6017ced952bca7aa91f1678ba1185dc3c11c0e
Md. Shahedul Islam <mdshahedulislamsaim@gmail.com>
2025-09-11 20:18:57 +0600

    1010.c

--- 1010.c
+++ 1010.c
@@ -2,8 +2,8 @@
 
 int main()
 {
-    int code1,unit1,code2,unit2,res;
-    float price1,price2;
+    int code1,unit1,code2,unit2;
+    float price1,price2,res;
 
     scanf("%d%d%f",&code1,&unit1,&price1);
     scanf("%d%d%f",&code2,&unit2,&price2);
